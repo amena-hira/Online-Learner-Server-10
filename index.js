3c73155a57fd3029ac8052a11dad2e9d3eccc510
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-19 19:10:46 -0800

    categories data show

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const categories = require('./data/categories.json');
+
 app.get('/', (req, res) => {
   res.send('Programming Language Course Server')
 })
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+  })
+
 
 app.listen(port, () => {
   console.log(`Programming Language Course Server running on port ${port}`)
