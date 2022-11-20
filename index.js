2c6969af6aeffe9b281363749a5a52ccef8822fc
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-19 20:19:57 -0800

    cors added and instaled for permission

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Programming Language Course Server')
